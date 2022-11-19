23e32370be06f561d49f0f3c1d09f6dba3debcd6
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2022-11-19 20:20:51 +0100

    second commit

--- index.js
+++ index.js
@@ -1,64 +1,86 @@
 const content = document.querySelector(".content");
 const addTaskBtn = document.querySelector("#addTaskBtn");
 const taskInput = document.querySelector("#taskInput");
-let count = 0;
-// addTask doda nowy h1 z unikalnym id i przyciskiem selfdelete/complete/Edit
+const tasksFromStorage = Object.values(localStorage);
 
-//Edit = readonly=false + toggluje przycisk edit na 'finish edytowanie'
-const addTask = () => {
-	count++;
+const loadTasks = function () {
+	var values = [];
+	keys = Object.keys(localStorage);
+	i = keys.length;
+
+	while (i--) {
+		values.push(localStorage.getItem(keys[i]));
+	}
+	console.log(values);
+};
+const addTask = function () {
 	let taskContent = taskInput.value;
 	taskInput.value = "";
+
 	content.insertAdjacentHTML(
 		"beforeend",
-		`<h1><input type='text' readonly="readonly"  value='${taskContent}'/>
-        <button id=task-delete-${count}>🗑️</button>
-        <button id=task-edit-${count}>🚧</button>
-        <button id=task-complete-${count}>✅</button>
+		`<h1 id=task-${taskContent}><input type='text' readonly="readonly"  value='${taskContent}'/>
+        <button id=task-delete-${taskContent}>🗑️</button>
+        <button id=task-edit-${taskContent}>🚧</button>
+        <button id=task-complete-${taskContent}>✅</button>
         </h1>`
 	);
-	document.querySelector(`#task-delete-${count}`).onclick = function () {
+	let task = document.querySelector(`#task-${taskContent}`);
+	let taskDelete = document.querySelector(`#task-delete-${taskContent}`);
+	let taskComplete = document.querySelector(`#task-complete-${taskContent}`);
+	let taskEdit = document.querySelector(`#task-edit-${taskContent}`);
+
+	localStorage.setItem(task.id, task.id);
+	localStorage.setItem(`${task.id}-isCompleted`, false);
+	// taskComplete.textContent === "✅"
+	// 	? (isCompleted = false)
+	// 	: (isCompleted = true);
+	// localStorage.setItem(`${this.parentNode.children[0].value}`, [
+	// 	document.querySelector(`#task-${count} > input[type=text]`).value,
+	// 	isCompleted,
+	// ]);
+
+	taskDelete.onclick = function () {
 		this.parentNode.remove();
-		count--;
+		localStorage.removeItem(task.id);
+		localStorage.removeItem(`${task.id}-isCompleted`);
 	};
-	document.querySelector(`#task-edit-${count}`).onclick = function () {
-		let currentTaskInput = this.parentNode.children[0];
-		let end = currentTaskInput.value.length;
-		currentTaskInput.toggleAttribute("readOnly");
-		currentTaskInput.setSelectionRange(end, end);
-		currentTaskInput.focus();
+	taskEdit.onclick = function () {
+		localStorage.removeItem(task.id);
+		localStorage.removeItem(`${task.id}-isCompleted`);
+
+		taskContent = this.parentNode.children[0];
+		//if editing, set task as undone
+		if (taskComplete.textContent === "❌") {
+			taskComplete.click();
+		}
+		//refocus cursor on input
+		let end = taskContent.value.length;
+		taskContent.toggleAttribute("readOnly");
+		taskContent.setSelectionRange(end, end);
+		taskContent.focus();
+
+		task.id = `task-${taskContent.value}`;
+		localStorage.setItem(task.id, task.id);
+		localStorage.setItem(`${task.id}-isCompleted`, false);
 	};
-	document.querySelector(`#task-complete-${count}`).onclick = function () {
+	taskComplete.onclick = function () {
 		if (this.textContent === "✅") {
 			this.parentNode.style.border = "2px solid green";
 			this.parentNode.style.backgroundColor = "lightgreen";
 			this.parentNode.children[0].style.backgroundColor = "lightgreen";
-			// console.log(this.parentNode.children);
 			this.textContent = "❌";
+			localStorage.setItem(`${task.id}-isCompleted`, true);
 			return;
 		}
 		this.parentNode.style.border = "2px solid black";
 		this.parentNode.style.backgroundColor = "white";
 		this.parentNode.children[0].style.backgroundColor = "white";
 		this.textContent = "✅";
+		localStorage.setItem(`${task.id}-isCompleted`, false);
 	};
-	saveLocalTodos();
 };
+document.addEventListener("DOMContentLoaded", (e) => {
+	loadTasks();
+});
 addTaskBtn.onclick = addTask;
-
-function saveLocalTodos(todo) {
-	let todos;
-	if (localStorage.getItem("todos") === null) {
-		todos = [];
-	} else {
-		todos = JSON.parse(localStorage.getItem("todos"));
-	}
-	todos.push(todo);
-	localStorage.setItem("todos", JSON.stringify(todos));
-}
-/*
-<h1>
-				<input type="text" placeholder="Type in the task..." />
-				<button id="addBtn">➕</button>
-			</h1>
-            */
